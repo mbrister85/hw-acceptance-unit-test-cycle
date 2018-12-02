ac4378f0b861a56cc1f5bd1ff6e02c85151d0169
mbrister <mbrister@citadel.edu>
2018-12-02 23:48:03 +0000

    finsished working

---
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -0,0 +1,51 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create!(movie)
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+  end
+end
+
+Then /the director of "(.*)" should be "(.*)"/ do |movie, director|
+  step %Q{I should see "#{movie}"}
+  step %Q{I should see "#{director}"}
+end
+
+
+Then /(.*) seed movies should exist/ do | n_seeds |
+  Movie.count.should be n_seeds.to_i
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  regexp  = /#{e1}.*#{e2}/m 
+  page.body.should =~  regexp
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  ratings = rating_list.split(%r{,\s*})
+  if uncheck
+    ratings.each do |r|
+      step "I uncheck #{r}"
+    end
+  else
+    ratings.each do |r|
+      step "I check #{r}"
+    end
+  end
+end
+
+
+Then /I should see all the movies/ do
+  movies = Movie.all
+end
